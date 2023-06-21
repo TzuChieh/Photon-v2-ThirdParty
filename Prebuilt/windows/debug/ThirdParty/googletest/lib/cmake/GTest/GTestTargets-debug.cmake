4aeb3b77a30ef77c79c4b1fc2c54b1f22fd36fcc
T.C. Chang <b01502051@ntu.edu.tw>
2023-06-22 03:06:51 +0800

    set a _d postfix to googletest debug libs

--- Prebuilt/windows/debug/ThirdParty/googletest/lib/cmake/GTest/GTestTargets-debug.cmake
+++ Prebuilt/windows/debug/ThirdParty/googletest/lib/cmake/GTest/GTestTargets-debug.cmake
@@ -8,42 +8,42 @@ set(CMAKE_IMPORT_FILE_VERSION 1)
 # Import target "GTest::gtest" for configuration "Debug"
 set_property(TARGET GTest::gtest APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
 set_target_properties(GTest::gtest PROPERTIES
-  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/lib/gtest.lib"
-  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/gtest.dll"
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/lib/gtest_d.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/gtest_d.dll"
   )
 
 list(APPEND _IMPORT_CHECK_TARGETS GTest::gtest )
-list(APPEND _IMPORT_CHECK_FILES_FOR_GTest::gtest "${_IMPORT_PREFIX}/lib/gtest.lib" "${_IMPORT_PREFIX}/bin/gtest.dll" )
+list(APPEND _IMPORT_CHECK_FILES_FOR_GTest::gtest "${_IMPORT_PREFIX}/lib/gtest_d.lib" "${_IMPORT_PREFIX}/bin/gtest_d.dll" )
 
 # Import target "GTest::gtest_main" for configuration "Debug"
 set_property(TARGET GTest::gtest_main APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
 set_target_properties(GTest::gtest_main PROPERTIES
-  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/lib/gtest_main.lib"
-  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/gtest_main.dll"
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/lib/gtest_main_d.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/gtest_main_d.dll"
   )
 
 list(APPEND _IMPORT_CHECK_TARGETS GTest::gtest_main )
-list(APPEND _IMPORT_CHECK_FILES_FOR_GTest::gtest_main "${_IMPORT_PREFIX}/lib/gtest_main.lib" "${_IMPORT_PREFIX}/bin/gtest_main.dll" )
+list(APPEND _IMPORT_CHECK_FILES_FOR_GTest::gtest_main "${_IMPORT_PREFIX}/lib/gtest_main_d.lib" "${_IMPORT_PREFIX}/bin/gtest_main_d.dll" )
 
 # Import target "GTest::gmock" for configuration "Debug"
 set_property(TARGET GTest::gmock APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
 set_target_properties(GTest::gmock PROPERTIES
-  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/lib/gmock.lib"
-  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/gmock.dll"
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/lib/gmock_d.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/gmock_d.dll"
   )
 
 list(APPEND _IMPORT_CHECK_TARGETS GTest::gmock )
-list(APPEND _IMPORT_CHECK_FILES_FOR_GTest::gmock "${_IMPORT_PREFIX}/lib/gmock.lib" "${_IMPORT_PREFIX}/bin/gmock.dll" )
+list(APPEND _IMPORT_CHECK_FILES_FOR_GTest::gmock "${_IMPORT_PREFIX}/lib/gmock_d.lib" "${_IMPORT_PREFIX}/bin/gmock_d.dll" )
 
 # Import target "GTest::gmock_main" for configuration "Debug"
 set_property(TARGET GTest::gmock_main APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
 set_target_properties(GTest::gmock_main PROPERTIES
-  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/lib/gmock_main.lib"
-  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/gmock_main.dll"
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/lib/gmock_main_d.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/gmock_main_d.dll"
   )
 
 list(APPEND _IMPORT_CHECK_TARGETS GTest::gmock_main )
-list(APPEND _IMPORT_CHECK_FILES_FOR_GTest::gmock_main "${_IMPORT_PREFIX}/lib/gmock_main.lib" "${_IMPORT_PREFIX}/bin/gmock_main.dll" )
+list(APPEND _IMPORT_CHECK_FILES_FOR_GTest::gmock_main "${_IMPORT_PREFIX}/lib/gmock_main_d.lib" "${_IMPORT_PREFIX}/bin/gmock_main_d.dll" )
 
 # Commands beyond this point should not need to know the version.
 set(CMAKE_IMPORT_FILE_VERSION)
