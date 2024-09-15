261aa3efd886a030fe36bc198bfd1cdb1ee385f4
Tzu-Chieh Chang <r05922176@ntu.edu.tw>
2024-09-16 05:55:55 +0800

    correctly iterate through python dict

--- Prebuilt/unpack.py
+++ Prebuilt/unpack.py
@@ -14,6 +14,6 @@ for prebuilt_type in prebuilt_types:
     with open(prebuilt_meta, 'r') as meta_file:
         symlink_src_to_dst = json.loads(meta_file.read())
 
-    for symlink_src, symlink_dst in symlink_src_to_dst:
+    for symlink_src, symlink_dst in symlink_src_to_dst.items():
         Path(symlink_src).symlink_to(Path(symlink_dst))
     print(f"restored {len(symlink_src_to_dst)} symlinks for {prebuilt_type}")
